f3e32876718f4b9b3b61f4a03b83f796c36f0225
Dom Trott <dominic.trott@youview.com>
2022-01-06 17:19:05 +0000

    Add IconInput component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,21 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZE = {
+  small: {
+    fontSize: 14/16,
+    lineThickness: 1,
+    height: 24,
+    iconSize: 16,
+  },
+  large: {
+    fontSize: 18/16,
+    lineThickness: 2,
+    height: 36,
+    iconSize: 24,
+  },
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +28,69 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+
+  const style = SIZE[size]
+
+  return (
+    <Wrapper style={{'--font-size': style.fontSize + 'rem'}}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{'--iconSize': style.iconSize + 'px'}}>
+        <Icon id={icon} size={style.iconSize} strokeWidth={style.lineThickness}></Icon>
+      </IconWrapper>
+      <NativeInput style={{
+        '--line-thickness': style.lineThickness + 'px',
+        '--height': style.height + 'px'
+      }} placeholder={placeholder} width={width}>
+
+      </NativeInput>
+    </Wrapper>
+  )
 };
+
+const Wrapper = styled.label`
+  position: block;
+  position: relative;
+
+  font-size: var(--font-size);
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  height: var(--iconSize);
+  margin: auto 0;
+`
+
+const NativeInput = styled.input`
+  display: inline-block;
+  border: none;
+  width: ${props => props.width}px;
+  height: var(--height);
+  border-bottom: var(--line-thickness) solid black;
+  padding-left: var(--height);
+
+  font-size: inherit;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  color: ${COLORS.gray700};
+
+  outline-offset: 2px;
+
+  &::placeholder {
+    font-weight: normal;
+  color: ${COLORS.gray500};
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
 
 export default IconInput;
